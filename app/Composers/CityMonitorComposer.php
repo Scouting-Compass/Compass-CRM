202ad06da5fd252c11ada393e7666f5ce5f1ab1e
Tim Joosten <Tjoosten3@gmail.com>
2018-10-07 22:39:28 +0200

    Update

---
+++ app/Composers/CityMonitorComposer.php
@@ -0,0 +1,71 @@
+<?php 
+
+namespace ActivismeBe\Composers;
+
+use Illuminate\View\View;
+use ActivismeBe\Models\City;
+ 
+/**
+ * Class CityMonitorComposer
+ * 
+ * @package ActivismeBe\Composers
+ */
+class CityMonitorComposer 
+{
+    /**
+     * Method for binding to the view. 
+     * 
+     * @param  View $view The builder instance for the blade view.
+     * @return void
+     */
+    public function compose(View $view): void 
+    {
+        $view->with('accepted', $this->acceptedCount()); 
+        $view->with('rejected', $this->rejectedCount());
+        $view->with('pending',  $this->pendingCount());
+    }
+
+    /**
+     * Counter function for the cities that accepted the charter. 
+     * 
+     * @return string
+     */
+    private function acceptedCount(): string
+    {
+        $total = $this->formatTotal();
+        return str_replace(',', '.', $total);
+    }
+
+    /**
+     * Counter function for the cities that rejected the charter. 
+     * 
+     * @return string
+     */
+    private function rejectedCount(): string
+    {
+        $total = $this->formatTotal(); 
+        return str_replace(',', '.', $total);
+    }
+
+    /**
+     * Counter function for the cities that are pending about the charter. 
+     * 
+     * @return string
+     */
+    private function pendingCount(): string
+    {
+        $total = $this->formatTotal(City::currentStatus('pending')->count());
+        return str_replace(',', '.', $total);
+    }
+
+    /**
+     * Function for formatting the output number. 
+     * 
+     * @param  int $total the total amount of cities with the given status. 
+     * @return string
+     */
+    private function formatTotal(int $total = 0): string 
+    {
+        return number_format($total);
+    }
+}
