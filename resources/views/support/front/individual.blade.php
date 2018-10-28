89a572ffbce733cbc415498b8e8ab700accab51a
Tim Joosten <Tjoosten3@gmail.com>
2018-10-28 23:31:40 +0100

    Update

--- resources/views/support/front/individual.blade.php
+++ resources/views/support/front/individual.blade.php
@@ -1,5 +1,10 @@
-@extends('layouts.frontend')
+@extends('layouts.frontend', ['py' => 'py-0'])
 
 @section('content')
-    
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Support us</h1>
+            <p class="lead">Support to achieve that belguim sign the Threaty that bans nuclear weapons.</p>
+        </div>
+    </div>
 @endsection
