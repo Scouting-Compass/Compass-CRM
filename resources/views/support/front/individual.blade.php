288c0ae712ade82bf308f4daec992010b715e1a4
Tim Joosten <Tjoosten3@gmail.com>
2018-10-29 07:24:23 +0100

    Update

--- resources/views/support/front/individual.blade.php
+++ resources/views/support/front/individual.blade.php
@@ -4,7 +4,10 @@
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
             <h1 class="display-4">Support us</h1>
-            <p class="lead">Support to achieve that belguim sign the Threaty that bans nuclear weapons.</p>
+            <p class="lead">Support us to achieve that belguim sign the Threaty that bans nuclear weapons.</p>
         </div>
+    </div>
+
+    <div class="container">
     </div>
 @endsection
